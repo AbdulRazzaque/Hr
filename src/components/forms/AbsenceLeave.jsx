76cef03a9f1433a358447d1669a93367334ac627
mhaslairazzaque@gmail.com <mhaslairazzaque@gmail.com>
2025-01-11 16:05:06 +0300

    mak api Absence leave

--- src/components/forms/AbsenceLeave.jsx
+++ src/components/forms/AbsenceLeave.jsx
@@ -35,18 +35,6 @@ const AbsenceLeave = () => {
   const [leaveStartDate, setleaveStartDate] = useState(null);
   const [LeaveEndDate, setLeaveEndDate] = useState(null);
   const [totalLeaveDays, setTotalLeaveDays] = useState(null);
-  const [lastNumberOfLeave,setLastNumberOfLeave]= useState("")
-
-  const [bankLoan, setBankLoan] = useState("No");
-  const [personalLoan, setPersonalLoan] = useState("No");
-  const [creditCard, setCreditCard] = useState("No");
-  const [companyLoan, setCompanyLoan] = useState("No");
-  const [companyAssets, setCompanyAssets] = useState("No");
-  const [mobileSimCard, setMobileSimCard] = useState("No");
-  const [laptop, setLaptop] = useState("No");
-  const [tools, setTools] = useState("No");
-  const [lastLeaveStartDate, setLastLeaveStartDate] = useState(null);
-  const [lastLeaveEndDate, setLastLeaveEndDate] = useState(null)
   const [leaveInfo, setLeaveInfo] = useState(null)
 
   
@@ -75,7 +63,7 @@ const getAllEmployeeData =()=>{
   
   },[])
 
-console.log(lastNumberOfLeave)
+
 //  =========================================Post api=========================================================
 const onSubmit = async(data,event)=>{
   // Check if leaveType is selected
@@ -106,27 +94,10 @@ const onSubmit = async(data,event)=>{
     formData.append("leaveType",leaveType)
     formData.append("leaveStartDate",leaveStartDate)
     formData.append("leaveEndDate",LeaveEndDate)
-    formData.append("numberOfDayLeave",totalLeaveDays)
-    if(leaveInfo){
-      formData.append("lastLeaveStartDate",leaveInfo.leaveStartDate|| lastLeaveStartDate)
-      formData.append("lastLeaveEndDate", leaveInfo.leaveEndDate ||lastLeaveEndDate)
-      formData.append("lastNumberOfDayLeave", leaveInfo?.numberOfDayLeave||lastNumberOfLeave)
-    }else{
-      formData.append("lastLeaveStartDate", lastLeaveStartDate)
-      formData.append("lastLeaveEndDate", lastLeaveEndDate)
-      formData.append("lastNumberOfDayLeave",lastNumberOfLeave)
-    }
+    formData.append("totalSickLeaveDays",totalLeaveDays)
 
+    
  
-
-    formData.append("bankLoan",bankLoan)
-    formData.append("personalLoan",personalLoan)
-    formData.append("CreditCard",creditCard)
-    formData.append("companyAssets",companyAssets)
-    formData.append("companyAssetsLoan",companyLoan)
-    formData.append("companySimCard",mobileSimCard)
-    formData.append("companyLaptop",laptop)
-    formData.append("tools",tools)
 
   
  
@@ -253,265 +224,9 @@ const rows = [
   { id: 8, lastName: 'Frances', firstName: 'Rossini', age: 36 },
   { id: 9, lastName: 'Roxie', firstName: 'Harvey', age: 65 },
 ];
-// console.log(selectedEmployee)
+
   return (
-    // <div className="row">
-    //   <div className="col-xs-12 col-sm-12 col-md-2 col-lg-2 col-xl-2">
-    //     <Dashhead id={3} display={display} />
-    //   </div>
-
-    //   <div
-    //     className="col-xs-12 col-sm-12 col-md-10 col-lg-10 col-xl-10 dashboard-container"
-    //     onClick={() => display && setDisplay(false)}
-    //   >
-    //     <span className="iconbutton display-mobile">
-    //       <IconButton
-    //         size="large"
-    //         aria-label="Menu"
-    //         onClick={() => setDisplay(true)}
-    //       >
-    //         <MenuIcon fontSize="inherit" />
-    //       </IconButton>
-    //     </span>
-    //     <form onSubmit={handleSubmit(onSubmit)}>
-
-    // <ToastContainer />
-    //     <div className="container-fluid">
-    //       <h1 className="mt-3 title text-center">
-    //       <Backicon/>
-    //         Exit For Leave
-    //       </h1>
-    //       <div className="icon-container">
-    //             <img src={exit}  alt="File icon" className="center headingimage mt-3" draggable="false"/>
-    //         </div>
-    //       <p className="subTitle">Exit Permit Request</p>
-    //       {/* ---------------------------------------------------First Row Start Here------------------------------------------- */}
-    //       <div className="row my-2">
-    //         <div className="col-4">
-    //           <LocalizationProvider dateAdapter={AdapterDayjs}>
-    //             <DatePicker
-    //               sx={{ width: 300 }}
-    //               label="Date"
-    //               value={date}
-    //                  format="DD/MM/YYYY"
-    //               onChange={(newValue) => setDate(newValue)}
-    //               renderInput={(params) => (
-    //                 <TextField name="date" {...params} />
-    //               )}
-    //             />
-    //           </LocalizationProvider>
-    //         </div>
-    //         <div className="col-8">
-    //         <Autocomplete
-    //           disablePortal
-    //           // sx={{ width: 500 }}
-    //              format="DD/MM/YYYY"
-    //           fullWidth
-    //           id="combo-box-demo"
-    //           options={data}
-    //           value={selectedEmployee}
-    //           getOptionLabel={(option) => option.name || ""} // Display employee name
-    //           // onChange={(event, value) => {
-            
-    //           // }}
-    //           onChange={(event,value)=>handleEmployee(event,value)}
-    //           renderInput={(params) => <TextField {...params} label="Select Employee Name" required/>}
-    //         />
-    //         </div>
-    //       </div> 
-    //       {/* ---------------------------------------------------Second Row Start Here------------------------------------------- */}
-    //       <div className="row my-5">
-    //         <div className="col">
-    //               <TextField
-    //             fullWidth
-    //             id="Position"
-    //             readOnly
-    //             variant="outlined" // Use "outlined" or "filled" variant to prevent label overlap
-    //             value={selectedEmployee?.employeeNumber ||""} // Dynamically update position
-    //             placeholder="Employee Number"
-    //             label= "Employee Number"
-    //             InputProps={{
-    //               readOnly: true, // Make position field read-only
-    //             }}
-    //           />  
-    //         </div>
-    //         <div className="col">
-    //         <TextField
-    //             fullWidth
-    //             id="Position"
-    //             readOnly
-    //             variant="outlined" // Use "outlined" or "filled" variant to prevent label overlap
-    //            label="Qatar ID"
-    //             placeholder="Qatar ID"
-    //             value={selectedEmployee?.qatarID||""} // Dynamically update position
-
-    //             InputProps={{
-    //               readOnly: true, // Make position field read-only
-    //             }}
-    //           /> 
-    //         </div>
-    //         <div className="col">
-    //         <TextField
-    //             fullWidth
-    //             id="Position"
-    //             readOnly
-    //             variant="outlined" // Use "outlined" or "filled" variant to prevent label overlap
-    //            label="Passport Number"
-    //             placeholder="Employee Number"
-    //             value={selectedEmployee?.passportNumber||""} // Dynamically update position
-    //             InputProps={{
-    //               readOnly: true, // Make position field read-only
-    //             }}
-    //           /> 
-    //         </div>
-    //       </div> 
-    //       {/* ------------------------------------------------------Third Row Start Here-------------------------------------------------------------------------- */}
-    //       <div className="row mt-4">
-
-    //           <div className="col-auto">
-    //                   <FormControl required error={!leaveType}>
-    //       <FormLabel id="demo-radio-buttons-group-label" className="font-weight-bold">Leave Type:</FormLabel>
-    //       <RadioGroup row
-    //         aria-labelledby="demo-radio-buttons-group-label"
-    //         defaultValue="female"
-    //         name="radio-buttons-group"
-    //         value={leaveType} // Bind to state
-    //         onChange={handleLeaveTypeChange} // Handle change event
-    //       >
-    //       <FormControlLabel value="Absent" control={<Radio />} label="Absent" />
-         
-     
-    //     </RadioGroup>
-    //   </FormControl>
-     
-    //           </div>
-        
-    //           <div className="col-4">
-    //         <TextField
-    //             fullWidth
-    //             id="Position"
-    //             readOnly
-    //             variant="outlined" // Use "outlined" or "filled" variant to prevent label overlap
-    //            label="Passport Number"
-    //             placeholder="Employee Number"
-    //             value={selectedEmployee?.passportNumber||""} // Dynamically update position
-    //             InputProps={{
-    //               readOnly: true, // Make position field read-only
-    //             }}
-    //           /> 
-    //         </div>
-    //         </div>
-
-    //       {/* ---------------------------------------------Forth row Start Here--------------------------------------------------- */}
-    //       <div className="row my-5">
-    //       <div className="col-auto">
-    //                   <FormControl required error={!leaveType}>
-    //       <FormLabel id="demo-radio-buttons-group-label" className="font-weight-bold">Leave Type:</FormLabel>
-    //       <RadioGroup row
-    //         aria-labelledby="demo-radio-buttons-group-label"
-    //         defaultValue="female"
-    //         name="radio-buttons-group"
-    //         value={leaveType} // Bind to state
-    //         onChange={handleLeaveTypeChange} // Handle change event
-    //       >
-      
-    //       <FormControlLabel value="sick" control={<Radio />} label="Sick" />
-     
-    //     </RadioGroup>
-    //   </FormControl>
-    //           </div>
-    //         <div className="col-auto">
-
-    //           <LocalizationProvider dateAdapter={AdapterDayjs}>
-    //             <DatePicker
-    //               className="mt-4"
-    //               sx={{ width: 200 }}
-    //               label="Leave Start  Date"
-    //               value={leaveStartDate}
-    //                  format="DD/MM/YYYY"
-    //               onChange={(newValue) => setleaveStartDate(newValue)}
-    //               renderInput={(params) => (
-    //                 <TextField name="date" {...params} />
-    //               )}
-    //             />
-    //           </LocalizationProvider>
-    //           </div>
-    //         <div className="col-auto">
-
-    //           <LocalizationProvider dateAdapter={AdapterDayjs}>
-    //             <DatePicker
-    //               className="mt-4"
-    //               sx={{ width: 200 }}
-    //               label="Leave End  Date"
-    //               value={LeaveEndDate}
-    //               onChange={(newValue) => setLeaveEndDate(newValue)}
-    //               format="DD/MM/YYYY"
-    //               renderInput={(params) => (
-    //                 <TextField name="date" {...params} />
-    //               )}
-    //             />
-    //           </LocalizationProvider>
-    //           </div>
-    //         <div className="col-auto mt-4">
-          
-    //                       <TextField
-    //             variant="outlined"
-    //             sx={{ width: 150 }}
-    //             value={totalLeaveDays}
-                
-    //             label="Total Number of Days of Leave"
-    //             InputLabelProps={{
-    //               shrink: true,  // Ensures the label stays above the input field
-    //             }}
-    //             InputProps={{
-    //               readOnly: true, // Make the field read-only since it's auto-calculated
-    //             }}
-    //           />
-    //           </div> 
-    //           <div className="col-auto">
-    //           <Paper sx={{ height: 1000,  }}>
-    //   <DataGrid
-    //     rows={rows}
-    //     columns={columns}
-    //     // initialState={{ pagination: { paginationModel } }}
-    //     // pageSizeOptions={[5, 10]}
-    //     // checkboxSelection
-    //     sx={{ border: 0 }}
-    //   />
-    // </Paper>
-    //             </div>      
-    //                 </div>
-    //       {/* ---------------------------------------------Fifth row Start Here--------------------------------------------------- */}
-    //       <div className="row my-5">
-           
-    //                 </div>
-    //                 <div className="row">
-    //                 <div className="col-8">
-    //                 <TextField
-    //                 rows={3}  // Sets the number of visible lines
-    //                 multiline={true} // Makes the field multiline
-    //                 fullWidth
-    //                 label="Comment"
-    //             id="outlined-basic"
-    //             // sx={{ width: 800 }}    label="Comment"
-    //             {...register("comment")}
-    //             variant="outlined"
-
-    //           />
-    //                 </div>
-    //                 </div>
-    
-
-        
-    //                  {/* --------------------------------Print Button---------------------------------------------------------- */}
-    //                  <Stack spacing={2} direction="row" marginBottom={2}  justifyContent="center">
-    //         <Button variant="contained" color="success" type="submit"> <SaveIcon className="mr-1"/> Save Form</Button>
-    //       {/* <Link to ="/EndofServicepdf">  <Button variant="contained"> <PrintIcon className="mr-1"/> Print Form</Button> </Link> */}
-    //         </Stack>
-    //     </div>
-    //     </form>
-    //   </div>
-    // </div>
+ 
 <div className="row">
   {/* Sidebar Section */}
   <div className="col-md-2 dashboard-sidebar">
@@ -662,7 +377,7 @@ const rows = [
               fullWidth
               label="Total Leave Days"
              
-          
+          {...register("totalAbsenceLeaveDays")}
             />
           </div>
           
